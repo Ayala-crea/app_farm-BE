358fa1fbb0c84f27b5e0b8758e5d378840844c1d
Ahmad Rifki Ayala <ayalarifki@gmail.com>
2024-11-06 18:42:52 +0700

    add helper for connect postgres

--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/aead/poly1305 v0.0.0-20180717145839-3fee0db0b635 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/pkg/errors v0.8.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
---
+++ helper/atdb/postgres.go
@@ -0,0 +1,20 @@
+package atdb
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func PostgresConnect(host string, port int, user, password, dbname, sslmode string) (*sql.DB, error) {
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		host, port, user, password, dbname, sslmode)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open connection: %v", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %v", err)
+	}
+	return db, nil
+}
